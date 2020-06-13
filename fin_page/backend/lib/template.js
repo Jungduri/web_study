9b78bf8d233553fd06b2b33a23ee110516cde997
jungduri <jcho7178@gmail.com>
2020-06-14 02:07:03 +0900

    [feat] 웹페이지 기능 추가 1. ejs, express 사용 2. 파일 읽어오고 동적으로 파일 리스트 출력 3. 업로드시 로컬에 파일 저장

--- fin_page/backend/lib/template.js
+++ fin_page/backend/lib/template.js
@@ -1,28 +1,40 @@
+var fs = require('fs');
+var parse5 = require('parse5');
+
 module.exports = {
-  HTML:function(title, list, body, control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },list:function(filelist){
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
+  readHTML:
+  function(user_name){
+    var path = `../../front/${user_name}.html`;
+    fs.readFile(path, 'utf8', function(err, description){
+      return description;
+    });
+  },
+  HTML:
+    function (html, fileNames) {
+      const document = parse5.parse(`<!DOCTYPE html>
+      <html>
+        <head>
+        </head>
+        <body>
+          <p>p1</p>
+          <p>p2</p>
+        </body>
+      </html>`);
+      var p3 = '<p>p3</p>'
+      var frag = document.childNodes[1].childNodes[2].childNodes[1].childNodes[0].value;
+      const new_doc = parse5.parseFragment(document.childNodes[1].childNodes[2].childNodes[1],p3)
+      console.log(new_doc)
+      //var files = this.FileList(fileNames);
+      return `
+      `;
+    },
+    FileList:
+    function(fileNames){
+      var list = ``;
+      var i = 0;
+      fileNames.forEach(fileName => {
+      list = list + `<ul>${fileName}</ul>`
+      });
     }
-    list = list+'</ul>';
-    return list;
-  }
+    
 }
