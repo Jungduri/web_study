91a5dd6cd57a683d8c21efef51a2b22fc01330b0
jung-seok Cho <jcho7178@gmail.com>
2020-06-17 22:47:41 +0900

    [feat] text 미리보기 기능 & 전체 적용 1. pdf 파일만 보이게끔 설정 2. text 파일도 함꼐 업로드해서 해당 이름과 같은 pdf파일만 보이게 설정

--- fin_page/backend/router/main.js
+++ fin_page/backend/router/main.js
@@ -1,39 +1,68 @@
-var multer = require('multer');
+var multer = require("multer");
 var storage = multer.diskStorage({
-   destination: function (req, file, cb) {
-      cb(null, `uploads/${file.fieldname}/`) // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
-   },
-   filename: function (req, file, cb) {
-      cb(null, file.originalname) // cb 콜백함수를 통해 전송된 파일 이름 설정
-   }
-})
-var uploads = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, `uploads/${file.fieldname}/`); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // cb 콜백함수를 통해 전송된 파일 이름 설정
+  },
+});
+var uploads = multer({ storage: storage });
 
-var fs = require('fs');
+var fs = require("fs");
+var path = require("path");
 
 module.exports = function (app) {
-   app.get('/', function (req, res) {
-      res.render('index')
-   });
-   app.get('/user', function (req, res) {
-      var user_name = req.query.id;
-      var dir_path = `./uploads/${user_name}/`;
-      fs.readdir(dir_path, (err, files) => {
-         res.render(`${user_name}`, {
-            files: files,
-            length: files.length
-         });
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+  app.get("/user", function (req, res) {
+    var user_name = req.query.id;
+    var dir_path = `./uploads/${user_name}/`;
+    var description = [];
+    var title = [];
+    fs.readdir(dir_path, (err, files) => {
+      files.forEach((file) => {
+        var file_name = path.parse(file).name;
+        var file_ext = path.parse(file).ext;
+        if (file_ext === ".pdf") {
+          title.push(file_name);
+          description.push(
+            fs.readFileSync(`${dir_path}${file_name}.txt`, "utf8")
+          );
+        }
       });
-   });
-   app.post('/uploads', uploads.single('jungseok'), function (req, res) {
-      console.log(req.file);
-      var user_name = req.file.fieldname;
-      var dir_path = `./uploads/${user_name}`;
-      fs.readdir(dir_path, (err, files) => {
-         res.render(`${user_name}`, {
-            files: files,
-            length: files.length
-         });
+      res.render(`${user_name}`, {
+        path: dir_path,
+        description: description,
+        title: title,
+        length: title.length,
       });
+    });
+  });
+  app.post("/uploads", uploads.single("jungseok"), 
+  function (req, res) {
+    var user_name = req.query.id;
+    var dir_path = `./uploads/${user_name}/`;
+    var description = [];
+    var title = [];
+    fs.readdir(dir_path, (err, files) => {
+      files.forEach((file) => {
+        var file_name = path.parse(file).name;
+        var file_ext = path.parse(file).ext;
+        if (file_ext === ".pdf") {
+          title.push(file_name);
+          description.push(
+            fs.readFileSync(`${dir_path}${file_name}.txt`, "utf8")
+          );
+        }
+      });
+      res.render(`${user_name}`, {
+        path: dir_path,
+        description: description,
+        title: title,
+        length: title.length,
+      });
+    });
    });
 }
